21dadf8a8b715ae2879a75562d8cbcf953daa71c
amit112amit <amit112amit@gmail.com>
2020-12-28 22:46:52 +0530

    Great Improvements.

--- gradeapp/helpers.py
+++ gradeapp/helpers.py
@@ -1,22 +1,25 @@
+import base64
 import numpy as np
 import pandas as pd
 
 
-def prepare_histogram_data_source(excelfile, maxmarks=100):
+def prepare_histogram_data_source(excelfilecontents, maxmarks=100):
     """
     Collect histogram details in a dictionary.
     """
+    excelfile = base64.b64decode(excelfilecontents)
     df = pd.read_excel(excelfile, engine='openpyxl')
-    marks = df['Total'].to_numpy()
+    marks = np.round(df['Total'].to_numpy(), 0)
     hist, bin_edges = np.histogram(marks, np.arange(-0.5, maxmarks + 1.5, 1))
     hist_data = {'top': hist, 'left': bin_edges[:-1], 'right': bin_edges[1:],
                  'bin_value': bin_edges[:-1] + 0.5}
     return hist_data
 
 
-def get_marks_stats(excelfile):
+def get_marks_stats(excelfilecontents):
     """
     Return the total number of students, the average marks and unique marks.
     """
+    excelfile = base64.b64decode(excelfilecontents)
     df = pd.read_excel(excelfile, engine='openpyxl')
     return df.shape[0], df.Total.mean()
--- gradeapp/main.py
+++ gradeapp/main.py
@@ -1,52 +1,39 @@
-import json
 from pathlib import Path
 from functools import partial
 from itertools import dropwhile
 
 from bokeh.layouts import column, row
-from bokeh.models import (CheckboxGroup, ColumnDataSource, Div, HoverTool, Label,
-                          Slider, Span)
+from bokeh.models import (Button, CheckboxGroup, ColumnDataSource, Div, FileInput,
+                          HoverTool, Label, Slider, Span, Spinner, TextInput)
 from bokeh.plotting import curdoc, figure
 
 from .helpers import get_marks_stats, prepare_histogram_data_source
 
-# Get the app directory name
-appdir = Path(__file__).parent
-
-# Read course name, Excel file name and the maximum marks
-with open(f'{appdir}/data/course_details.json', 'r') as inputfile:
-    course_details = json.load(inputfile)
-
-coursetitle = course_details['title']
-excelfilename = course_details['excelfile']
-maxmarks = course_details['maxmarks']
-
-marksfile = f'{appdir}/data/{excelfilename}'
-
-# Get total number of students and the average marks
-total_students, average_marks = get_marks_stats(marksfile)
-
-# Prepare histogram data source
-hist_data = prepare_histogram_data_source(marksfile, maxmarks)
-hist_source = ColumnDataSource(data=hist_data)
-scores = hist_data['bin_value']
-
-# Get the lowest score in the class
-for first_score, h in enumerate(hist_data['top']):
-    if h > 0:
-        break
-
-# Set the ranges
-xmin = scores[first_score] - 2
-xmax = maxmarks
-ymin = 0
-ymax = max(hist_source.data['top']) + 2
+# The "global" variables
+course_details = {'title': "BITS FXYZ Dummy Course", "maxmarks": 100,
+                  'total_students': 100, 'average_marks': 50}
+hist_source = ColumnDataSource(
+    data=dict(top=[], left=[], right=[], bin_value=[]))
 
 # Make the figure
-plot = figure(title=f'{coursetitle}', x_range=(xmin, xmax), y_range=(
-    ymin, ymax), sizing_mode='scale_both', tools=[])
+plot = figure(sizing_mode='scale_both', tools=[],
+              x_range=(0, course_details['maxmarks']), y_range=(0, 10))
 plot.xaxis.axis_label = "Score"
 plot.yaxis.axis_label = "Count"
+
+# Create a File Picker that accepts only Excel files.
+fileinput = FileInput(accept='.xlsx', multiple=False, align=('end', 'end'))
+
+# Create a TextInput for course title
+coursetitleinput = TextInput(
+    title='Course Title', placeholder='BITS FXYZ Dummy Course')
+
+# Create a Spinner for maximum marks
+maxmarksinput = Spinner(title='Maximum Marks', low=100,
+                        high=300, step=100, value=100)
+
+# Button to load histogram
+button = Button(label="Load", button_type='primary', align=('end', 'end'))
 
 # Create the tooltip
 hovertool = HoverTool(tooltips=[("Score", "@bin_value"),
@@ -57,37 +44,40 @@ plot.add_tools(hovertool)
 plot.quad(source=hist_source, top='top', bottom=0, left='left', right='right')
 
 # Add the average marker
-average_marker = Span(location=average_marks, dimension='height', line_color='red',
+average_marker = Span(location=course_details['average_marks'], dimension='height', line_color='red',
                       line_dash='dashed', line_width=3)
 plot.add_layout(average_marker)
 
+# Store maximum marks for easy access
+maxmarks = course_details['maxmarks']
+
 # Create a Span for each grade
-a_span = Span(location=80, dimension='height',
+a_span = Span(location=int(0.8 * maxmarks), dimension='height',
               line_color='green', line_width=3)
-am_span = Span(location=70, dimension='height', visible=False,
+am_span = Span(location=int(0.7 * maxmarks), dimension='height', visible=False,
                line_color='green', line_width=3)
-b_span = Span(location=60, dimension='height',
+b_span = Span(location=int(0.6 * maxmarks), dimension='height',
               line_color='green', line_width=3)
-bm_span = Span(location=50, dimension='height', visible=False,
+bm_span = Span(location=int(0.5 * maxmarks), dimension='height', visible=False,
                line_color='green', line_width=3)
-c_span = Span(location=40, dimension='height',
+c_span = Span(location=int(0.4 * maxmarks), dimension='height',
               line_color='green', line_width=3)
-cm_span = Span(location=30, dimension='height', visible=False,
+cm_span = Span(location=int(0.3 * maxmarks), dimension='height', visible=False,
                line_color='green', line_width=3)
-d_span = Span(location=20, dimension='height',
+d_span = Span(location=int(0.2 * maxmarks), dimension='height',
               line_color='green', line_width=3)
-e_span = Span(location=10, dimension='height', visible=False,
+e_span = Span(location=int(0.1 * maxmarks), dimension='height', visible=False,
               line_color='green', line_width=3)
 
 # Create a Label for each grade
-a_label = Label(x=80, y=ymax - 1, text=f"A: 0")
-am_label = Label(x=70, y=ymax - 1, text=f"A-: 0", visible=False)
-b_label = Label(x=60, y=ymax - 1, text=f"B: 0")
-bm_label = Label(x=50, y=ymax - 1, text=f"B-: 0", visible=False)
-c_label = Label(x=40, y=ymax - 1, text=f"C: 0")
-cm_label = Label(x=30, y=ymax - 1, text=f"C-: 0", visible=False)
-d_label = Label(x=20, y=ymax - 1, text=f"D: 0")
-e_label = Label(x=10, y=ymax - 1, text=f"E: 0", visible=False)
+a_label = Label(x=int(0.8 * maxmarks), y=9, text=f"A: 0")
+am_label = Label(x=int(0.7 * maxmarks), y=9, text=f"A-: 0", visible=False)
+b_label = Label(x=int(0.6 * maxmarks), y=9, text=f"B: 0")
+bm_label = Label(x=int(0.5 * maxmarks), y=9, text=f"B-: 0", visible=False)
+c_label = Label(x=int(0.4 * maxmarks), y=9, text=f"C: 0")
+cm_label = Label(x=int(0.3 * maxmarks), y=9, text=f"C-: 0", visible=False)
+d_label = Label(x=int(0.2 * maxmarks), y=9, text=f"D: 0")
+e_label = Label(x=int(0.1 * maxmarks), y=9, text=f"E: 0", visible=False)
 
 for l in (a_span, a_label, am_span, am_label, b_span, b_label,
           bm_span, bm_label, c_span, c_label, cm_span,
@@ -137,7 +127,7 @@ grades_data = {
     'A': {
         "Enabled": True,
         "Weight": 10,
-        "CutOff": 80,
+        "CutOff": int(0.8 * maxmarks),
         "Slider": a_slider,
         "Span": a_span,
         "Label": a_label
@@ -145,7 +135,7 @@ grades_data = {
     'A-': {
         "Enabled": False,
         "Weight": 9,
-        "CutOff": 70,
+        "CutOff": int(0.7 * maxmarks),
         "Slider": am_slider,
         "Span": am_span,
         "Label": am_label
@@ -153,7 +143,7 @@ grades_data = {
     'B': {
         "Enabled": True,
         "Weight": 8,
-        "CutOff": 60,
+        "CutOff": int(0.6 * maxmarks),
         "Slider": b_slider,
         "Span": b_span,
         "Label": b_label
@@ -161,7 +151,7 @@ grades_data = {
     'B-': {
         "Enabled": False,
         "Weight": 7,
-        "CutOff": 50,
+        "CutOff": int(0.5 * maxmarks),
         "Slider": bm_slider,
         "Span": bm_span,
         "Label": bm_label
@@ -169,7 +159,7 @@ grades_data = {
     'C': {
         "Enabled": True,
         "Weight": 6,
-        "CutOff": 40,
+        "CutOff": int(0.4 * maxmarks),
         "Slider": c_slider,
         "Span": c_span,
         "Label": c_label
@@ -177,7 +167,7 @@ grades_data = {
     'C-': {
         "Enabled": False,
         "Weight": 5,
-        "CutOff": 30,
+        "CutOff": int(0.3 * maxmarks),
         "Slider": cm_slider,
         "Span": cm_span,
         "Label": cm_label
@@ -185,7 +175,7 @@ grades_data = {
     'D': {
         "Enabled": True,
         "Weight": 4,
-        "CutOff": 20,
+        "CutOff": int(0.2 * maxmarks),
         "Slider": d_slider,
         "Span": d_span,
         "Label": d_label
@@ -193,12 +183,66 @@ grades_data = {
     'E': {
         "Enabled": False,
         "Weight": 2,
-        "CutOff": 10,
+        "CutOff": int(0.1 * maxmarks),
         "Slider": e_slider,
         "Span": e_span,
         "Label": e_label
     },
 }
+
+
+def load_course_data():
+    """
+    1. Process the Excel sheet.
+    2. Set the course title.
+    3. Set the maximum marks.
+    4. Update plot range and title
+    """
+    course_details['maxmarks'] = maxmarksinput.value
+    course_details['title'] = coursetitleinput.value
+    excelfilecontents = fileinput.value
+    hist_data = prepare_histogram_data_source(excelfilecontents, maxmarks)
+
+    # Get some stats
+    total_students, average_marks = get_marks_stats(excelfilecontents)
+    course_details['total_students'] = total_students
+    course_details['average_marks'] = average_marks
+
+    # Update the histogram column source
+    hist_source.data = hist_data
+
+    # Get the lowest score in the class
+    for first_score, h in enumerate(hist_data['top']):
+        if h > 0:
+            break
+
+    # Set the ranges
+    xmin = hist_data['bin_value'][first_score] - 2
+    ymax = max(hist_data['top']) + 2
+
+    # Update the label locations
+    a_label.y = ymax - 1
+    am_label.y = ymax - 1
+    b_label.y = ymax - 1
+    bm_label.y = ymax - 1
+    c_label.y = ymax - 1
+    cm_label.y = ymax - 1
+    d_label.y = ymax - 1
+    e_label.y = ymax - 1
+
+    # Update plot ranges, title and the average marker
+    plot.title.text = f"{course_details['title']} MGPA: 0.0"
+    plot.x_range.start = xmin
+    plot.x_range.end = course_details['maxmarks']
+    plot.y_range.end = ymax
+    average_marker.location = average_marks
+
+    # Update grade markers
+    update_plot()
+
+
+# Add callback to the button
+button.on_click(load_course_data)
 
 
 def calculate_stats():
@@ -214,7 +258,7 @@ def calculate_stats():
 
         # Count the number of students from histogram data
         lowerlimit = data['CutOff']
-        count = hist_data['top'][lowerlimit: upperlimit].sum()
+        count = sum(hist_source.data['top'][lowerlimit: upperlimit])
 
         # Update MGPA
         mgpa += count * data['Weight']
@@ -226,7 +270,7 @@ def calculate_stats():
         upperlimit = lowerlimit
 
     # Normalize the MGPA
-    mgpa /= total_students
+    mgpa /= course_details['total_students']
 
     return counts, mgpa
 
@@ -254,6 +298,7 @@ def update_plot():
 
             # Enable the slider
             data['Slider'].disabled = False
+            data['Slider'].value = data['CutOff']
         else:
             # Turn off the disabled grades
             data['Span'].visible = False
@@ -263,7 +308,7 @@ def update_plot():
             data['Slider'].disabled = True
 
     # Set the MGPA in the title
-    plot.title.text = f'{coursetitle}, MGPA:{round(mgpa, 2)}'
+    plot.title.text = f'{course_details["title"]} MGPA:{round(mgpa, 2)}'
 
 
 def enable_disable_grade(attr, old, new, grade=None):
@@ -275,7 +320,7 @@ def enable_disable_grade(attr, old, new, grade=None):
         grades_data[grade]['Enabled'] = True
     else:
         grades_data[grade]['Enabled'] = False
-    
+
     # Finally, update the plot
     update_plot()
 
@@ -353,11 +398,13 @@ d_slider.on_change("value", partial(update_grade_cutoff, grade='D'))
 e_slider.on_change("value", partial(update_grade_cutoff, grade='E'))
 
 # Add some introductory text
+appdir = Path(__file__).parent
 desc = Div(text=open(f"{appdir}/description.html").read())
 
 # Arrange all the controls nicely
-layout = column(desc, row(plot, column(a_row, am_row, b_row, bm_row, c_row,
-                                       cm_row, d_row, e_row)))
+layout = column(desc, row(coursetitleinput, maxmarksinput, fileinput, button),
+                row(plot, column(a_row, am_row, b_row, bm_row, c_row, cm_row,
+                                 d_row, e_row, align=('end', 'center'))))
 
 # Initialization of the plot
 update_plot()
